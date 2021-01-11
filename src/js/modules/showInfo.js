6bce22fac39af3467d2811659bd4f19d19628c1f
Pavel <pl.hancharou@gmail.com>
2021-01-11 13:53:59 +0300

    fix: nextElemSibling by btn

--- src/js/modules/showInfo.js
+++ src/js/modules/showInfo.js
@@ -6,7 +6,7 @@ export default class ShowInfo {
   init() {
     this.btns.forEach(btn => {
       btn.addEventListener('click', () => {
-        if (btn.closest('.module__info-show').nextElementSibling.style.display !== 'block') {
+        if (!btn.closest('.module__info-show') || btn.closest('.module__info-show').nextElementSibling.style.display !== 'block') {
           btn.parentNode.style.cssText = `transform: rotate(45deg); transition: all .3s`;
           btn.closest('.module__info-show').nextElementSibling.style.display = 'block';
         } else {
