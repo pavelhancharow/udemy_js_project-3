e232ef24fffa6a67317d090db3630c70ccd467ee
Pavel <pl.hancharou@gmail.com>
2021-01-11 12:49:05 +0300

    feat: create/add class ShowInfo

---
+++ src/js/modules/showInfo.js
@@ -0,0 +1,19 @@
+export default class ShowInfo {
+  constructor(triggers) {
+    this.btns = document.querySelectorAll(triggers);
+  }
+
+  init() {
+    this.btns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        if (btn.closest('.module__info-show').nextElementSibling.style.display !== 'block') {
+          btn.parentNode.style.cssText = `transform: rotate(45deg); transition: all .3s`;
+          btn.closest('.module__info-show').nextElementSibling.style.display = 'block';
+        } else {
+          btn.parentNode.style.cssText = `transform: rotate(0deg); transition: all .3s`;
+          btn.closest('.module__info-show').nextElementSibling.style.display = 'none';
+        }
+      });
+    });
+  }
+}
